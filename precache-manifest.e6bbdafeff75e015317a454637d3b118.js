fa814b2d6801e938f984ecfe2b8d369d77bf5809
keephacking <savigms2@gmail.com>
2019-08-16 02:16:38 +0530

    Updates

---
+++ precache-manifest.e6bbdafeff75e015317a454637d3b118.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ed019131812d22c6c163fd19b9aa32",
+    "url": "/react-gallery-exp/index.html"
+  },
+  {
+    "revision": "49339f7b1c9459190f5b",
+    "url": "/react-gallery-exp/static/css/2.c4183d6c.chunk.css"
+  },
+  {
+    "revision": "a84fe1fbe42efe82b651",
+    "url": "/react-gallery-exp/static/css/main.e47cd38f.chunk.css"
+  },
+  {
+    "revision": "49339f7b1c9459190f5b",
+    "url": "/react-gallery-exp/static/js/2.0f2e32e4.chunk.js"
+  },
+  {
+    "revision": "a84fe1fbe42efe82b651",
+    "url": "/react-gallery-exp/static/js/main.80d66e36.chunk.js"
+  },
+  {
+    "revision": "457ef0b359b8ed5c3910",
+    "url": "/react-gallery-exp/static/js/runtime~main.1c7c93f2.js"
+  },
+  {
+    "revision": "87b7cecd94ae076c2a7140c321e4452b",
+    "url": "/react-gallery-exp/static/media/flexslider-icon.87b7cecd.svg"
+  },
+  {
+    "revision": "9c9cb7a6055043933ba68854f521af45",
+    "url": "/react-gallery-exp/static/media/flexslider-icon.9c9cb7a6.eot"
+  },
+  {
+    "revision": "b4c9e5057989b9727a5df4e0a21af33c",
+    "url": "/react-gallery-exp/static/media/flexslider-icon.b4c9e505.ttf"
+  },
+  {
+    "revision": "f8b92f66539473eea649c8514eb836a0",
+    "url": "/react-gallery-exp/static/media/flexslider-icon.f8b92f66.woff"
+  }
+]);
